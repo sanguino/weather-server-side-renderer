61e9e68a93c7810281a5af411095bd187ea09f1c
Miguel G. Sanguino <sanguino@me.com>
2020-05-17 01:32:36 +0200

    refactor to use express to render the page

--- server.js
+++ server.js
@@ -1,50 +1,51 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
-const path = require('path');
-const app = express();
+const puppeteer = require('puppeteer')
+const express = require('express')
+const path = require('path')
+const app = express()
 
-const WIDTH = 128;
-const HEIGHT = 296;
+const WIDTH = 128
+const HEIGHT = 296
 
 function calculatePizels(pixels) {
-    let pixelsData = [];
-    let pixelsDataByte = '';
+    let pixelsData = []
+    let pixelsDataByte = ''
     for (let i = 0; i < pixels.length; i += 4) {
         pixelsDataByte += !!pixels[i]? '1': '0'
 
-        if (pixelsDataByte.length === 8) {
+        if (pixelsDataByte.length === 8) {
             pixelsData.push(parseInt(pixelsDataByte, 2))
             pixelsDataByte = ''
         }
     }
-    const buffer = new ArrayBuffer(pixelsData.length);
-    const uint8 = new Uint8Array(buffer);
+    const buffer = new ArrayBuffer(pixelsData.length)
+    const uint8 = new Uint8Array(buffer)
     uint8.set(pixelsData)
-    return uint8.toString();
+    return uint8.toString()
 }
 
 async function renderPixels(temp, hum, bat, usb) {
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
     await page.setViewport({ width: WIDTH, height: HEIGHT })
-    await page.goto(`file://${path.resolve('.')}/index.html?temp=${temp}&hum=${hum}&bat=${bat}&usb=${usb}`);
-    //  await page.setContent(htmlContent, {waitUntil: 'networkidle2'});
+    await page.goto(`http://localhost:3223/?temp=${temp}&hum=${hum}&bat=${bat}&usb=${usb}`)
     await page.waitForResponse(response => response.ok())
     const pixels = await page.screenshot()
     await browser.close()
-    return pixels;
+    return pixels
 }
 
-app.get('/', async (req, res) => {
+app.use('/', express.static('src'))
+
+app.get('/api', async (req, res) => {
     
     const pixels = await renderPixels(req.query.temp, req.query.hum, req.query.bat, req.query.usb)
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*")
     res.json({
         width: WIDTH,
         height: HEIGHT,
-        tick: 1000,//(60 - new Date().getSeconds())*1000,
+        tick: (60 - new Date().getSeconds())*1000,
         data: calculatePizels(pixels)
-    });
-});
+    })
+})
 
-app.listen(3223);
+app.listen(3223)
